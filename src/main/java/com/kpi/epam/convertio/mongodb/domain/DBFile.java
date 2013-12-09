717c6864236fcc66c62a7bc0b196e31e64694c98
Julia Valchuk <julia.valchuk@gmail.com>
2013-12-10 00:50:43 +0200

    domain add

---
+++ src/main/java/com/kpi/epam/convertio/mongodb/domain/DBFile.java
@@ -0,0 +1,30 @@
+package com.kpi.epam.convertio.mongodb.domain;
+
+
+/**
+ * Created by Julia on 06.12.13.
+ */
+
+public class DBFile {
+    public final String hash;
+    public final String way;
+
+    public DBFile(String hash, String way) {
+        this.way = way;
+        this.hash = hash;
+    }
+
+    public DBFile(Integer hash, String way) {
+        this.way = way;
+        this.hash = hash.toString();
+
+    }
+
+    @Override
+    public String toString() {
+        return "{\n" +
+                "\thash : \"" + hash + "\", \n" +
+                "\tway: \"" + way + "\", \n" +
+                "}";
+    }
+}
